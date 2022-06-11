b7f14ab037d3518bb824c18ffa16ead574368c4a
Medusiora <khom@outsourcify.net>
2022-06-11 20:00:23 +0700

    [feat] add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
-  <div class="container">
-    <img alt="Vue logo" src="@/assets/images/logo.png" class="mx-auto" />
-    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  </div>
+  <Header></Header>
+  <router-view></router-view>
+  <Footer></Footer>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "@/views/Home.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import('@/views/Login.vue'),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
