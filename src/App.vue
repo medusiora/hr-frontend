da079cc255d8206c56cecf4b20100e187f8178fb
Medusiora <khom@outsourcify.net>
2022-06-11 22:43:48 +0700

    [feat] implement layout component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Header from '@/components/Header.vue';
-import Footer from '@/components/Footer.vue';
+import Layout from '@/layouts/default.vue';
 </script>
 
 <template>
-  <Header />
-  <RouterView />
-  <Footer />
+  <Layout />
 </template>
